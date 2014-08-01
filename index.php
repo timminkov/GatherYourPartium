12a379a86b9109b4335a6f7f6400ce9971357921
Tim Minkov <timothyminkov@gmail.com>
2014-08-01 00:39:04 -0700

    [CLEANUP] Sane spacing in index.php

--- index.php
+++ index.php
@@ -3,70 +3,66 @@
 <img id="background" src="<?php bloginfo('template_url'); ?>/img/defaultbgv4.jpg">
 
 <a href="<?php echo site_url(); ?>" class="title">
-    <img id="logo" src="<?php bloginfo('template_url'); ?>/img/placeholderlogo.png">
+  <img id="logo" src="<?php bloginfo('template_url'); ?>/img/placeholderlogo.png">
 </a>
-    <h2>Gather <em>Your</em> Party</h2>
+
+<h2>Gather <em>Your</em> Party</h2>
 
 <div id="strap">Just Video Games</div>
 
 <section id="main">
-    <div class="wrap">
+  <div class="wrap">
 
-<?php
-$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+    <?php
+    $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
 
-$args = array(
-    'tag__not_in' => convert_slug_to_id('bio'),
-    'posts_per_page' => 4,
-    'paged' => $paged,
-    'showposts' => 5,
-    'offset' =>(($paged-1)*10),
-);
-$wp_query = new WP_Query( $args ); // don't show user bios
-$wp_query->is_archive = false; $wp_query->is_home = true;
-while($wp_query->have_posts()) : $wp_query->the_post(); ?>
+    $args = array(
+        'tag__not_in' => convert_slug_to_id('bio'),
+        'posts_per_page' => 4,
+        'paged' => $paged,
+        'showposts' => 5,
+        'offset' =>(($paged-1)*10),
+    );
+    $wp_query = new WP_Query( $args ); // don't show user bios
+    $wp_query->is_archive = false; $wp_query->is_home = true;
+    while($wp_query->have_posts()) : $wp_query->the_post(); ?>
 
-<?php get_template_part( 'frontpost', 'large' ); ?>
+    <?php get_template_part( 'frontpost', 'large' ); ?>
 
-<?php endwhile; ?>
-<?php wp_reset_postdata(); // reset the query ?>
+    <?php endwhile; ?>
+    <?php wp_reset_postdata(); // reset the query ?>
 
-        <div class="older-articles">
+    <div class="older-articles">
 
-<?php
+      <?php
+        $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
 
-$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+        $args = array(
+            'tag__not_in' => convert_slug_to_id('bio'),
+            'posts_per_page' => 6,
+            'paged' => $paged,
+            'showposts' => 5,
+            'offset' => (($paged-1)*10)+5,
+        );
 
-$args = array(
-    'tag__not_in' => convert_slug_to_id('bio'),
-    'posts_per_page' => 6,
-    'paged' => $paged,
-    'showposts' => 5,
-    'offset' => (($paged-1)*10)+5,
-);
+        $wp_query = new WP_Query($args); // don't show user bios
+        $wp_query->is_archive = false; $wp_query->is_home = true;
+        while($wp_query->have_posts()) : $wp_query->the_post(); ?>
 
-$wp_query = new WP_Query($args); // don't show user bios
-$wp_query->is_archive = false; $wp_query->is_home = true;
-while($wp_query->have_posts()) : $wp_query->the_post(); ?>
+      <?php get_template_part( 'frontpost', 'small' ); ?>
+      <?php endwhile; ?>
+      <?php wp_reset_postdata(); // reset the query ?>
+      <?php get_template_part( 'pagination' ); ?>
 
-<?php get_template_part( 'frontpost', 'small' ); ?>
-
-
-<?php endwhile; ?>
-<?php wp_reset_postdata(); // reset the query ?>
-
-<?php get_template_part( 'pagination' ); ?>
-
-        </div>
-
+    </div>
 
     <div id="sidebar" class="sidebar">
-    <?php if ( is_active_sidebar( 'sidebar-front' ) ) : ?>
-    <?php dynamic_sidebar( 'sidebar-front' ); ?>
-    <?php endif; ?>
+      <?php if ( is_active_sidebar( 'sidebar-front' ) ) : ?>
+      <?php dynamic_sidebar( 'sidebar-front' ); ?>
+      <?php endif; ?>
     </div>
 
-    </div>
+  </div>
 
 </section>
 
