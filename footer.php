6e067809998e92b4625cb3c3a513016562f99336
Tim Minkov <timothyminkov@gmail.com>
2014-08-01 11:16:04 -0700

    [CLEANUP] Spacing in footer.php

--- footer.php
+++ footer.php
@@ -1,41 +1,33 @@
 <footer id="footer">
+  <div class="wrap">
+    <section id="footer-siteinfo">
+      <p>Design and development by <a href="http://lunchtime.org.uk">Rob Welch</a> with help from <a href="http://www.cenix.co.uk">Daniel Morgan</a>. Logo by <a href="http://livewareblog.wordpress.com/">Aleksander Adamkiewicz</a>. Social Icons by <a href="http://ctrla.lt/">Ctrl+Alt+Design</a>.</p>
+      <p>All content is owned by the author. Site design released under a <a href="<?php bloginfo('template_url'); ?>/license.txt">KYGMOYGPL</a> license. GYP is run by volunteers, <a href="<?php get_home_url(); ?>/apply">find out how you can get involved!</a></p>
+    </section>
 
-    <div class="wrap">
-
-        <section id="footer-siteinfo">
-            <p>Design and development by <a href="http://lunchtime.org.uk">Rob Welch</a> with help from <a href="http://www.cenix.co.uk">Daniel Morgan</a>. Logo by <a href="http://livewareblog.wordpress.com/">Aleksander Adamkiewicz</a>. Social Icons by <a href="http://ctrla.lt/">Ctrl+Alt+Design</a>.</p>
-            <p>All content is owned by the author. Site design released under a <a href="<?php bloginfo('template_url'); ?>/license.txt">KYGMOYGPL</a> license. GYP is run by volunteers, <a href="<?php get_home_url(); ?>/apply">find out how you can get involved!</a></p>
-        </section>
-
-        <section id="footer-links">
-            <ul id="footer-links-list">
-                <li><a href="https://github.com/Catsman/GatherYourPartium/issues">Issue Tracker</a> (find a bug?)</li>
-                <li><a href="https://github.com/Catsman/GatherYourPartium">Source Code</a> (fork us!)</li>
-                <li><a href="mailto:kyle@gatheryourparty.com">Contact us</a> (tips/corrections?)</li>
-                <li><a href="<?php get_home_url(); ?>/apply">Contribute to GYP!</a></li>
-                <li><a href="<?php get_home_url(); ?>/wp-admin">Login</a></li>
-            </ul>
-
-        </section>
-
-    </div>
-
+    <section id="footer-links">
+      <ul id="footer-links-list">
+        <li><a href="https://github.com/Catsman/GatherYourPartium/issues">Issue Tracker</a> (find a bug?)</li>
+        <li><a href="https://github.com/Catsman/GatherYourPartium">Source Code</a> (fork us!)</li>
+        <li><a href="mailto:kyle@gatheryourparty.com">Contact us</a> (tips/corrections?)</li>
+        <li><a href="<?php get_home_url(); ?>/apply">Contribute to GYP!</a></li>
+        <li><a href="<?php get_home_url(); ?>/wp-admin">Login</a></li>
+      </ul>
+    </section>
+  </div>
 </footer>
 
 <?php wp_footer(); ?>
 
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="<?php bloginfo('template_url'); ?>js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="<?php bloginfo('template_url'); ?>js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+<script src="<?php bloginfo('template_url'); ?>/js/plugins.js"></script>
+<script>
+  var _gaq=[['_setAccount','UA-30762260-1'],['_trackPageview']];
+  (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+  g.src='//www.google-analytics.com/ga.js';
+  s.parentNode.insertBefore(g,s)}(document,'script'));
+</script>
 
-        <script src="<?php bloginfo('template_url'); ?>/js/plugins.js"></script>
-
-        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID.         -->
-        <script>
-            var _gaq=[['_setAccount','UA-30762260-1'],['_trackPageview']];
-            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-            g.src='//www.google-analytics.com/ga.js';
-            s.parentNode.insertBefore(g,s)}(document,'script'));
-        </script>
-
-    </body>
+  </body>
 </html>
