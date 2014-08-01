b4a7da734d47f70250f78913c6f4c0ec899a96be
Tim Minkov <timothyminkov@gmail.com>
2014-08-01 16:54:21 -0700

    [CLEANUP] Refactor templates to shared templates

--- videos.php
+++ videos.php
@@ -1,63 +1,8 @@
 <?php
-/*
-Template Name: Videos
-*/
-$test = 'Videos'
+  /*
+  Template Name: Videos
+  */
+  $template_name = 'Videos';
+  $tag_name = 'video';
+  include(locate_template('partials/page_template.php'));
 ?>
-
-<?php get_header(); ?>
-
-<?php get_template_part( 'partials/test' ); ?>
-
-<section id="main">
-  <div class="wrap">
-
-    <?php
-      $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-
-      $args = array(
-          'tag__not_in' => convert_slug_to_id('bio'),
-          'tag' => 'video',
-          'posts_per_page' => 10,
-          'paged' => $paged,
-          'showposts' => 5,
-          'offset' => (($paged-1)*10),
-      );
-      $wp_query = new WP_Query( $args ); // don't show user bios
-      $wp_query->is_archive = false; $wp_query->is_home = true;
-      while($wp_query->have_posts()) : $wp_query->the_post(); ?>
-
-    <?php get_template_part( 'frontpost', 'large' ); ?>
-
-    <?php endwhile; ?>
-    <?php wp_reset_postdata(); // reset the query ?>
-
-    <div class="older-articles">
-
-      <?php
-        $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-
-        $args = array(
-            'tag__not_in' => convert_slug_to_id('bio'),
-            'tag' => 'video',
-            'posts_per_page' => 10,
-            'paged' => $paged,
-            'showposts' => 5,
-            'offset' => (($paged-1)*10)+6,
-        );
-
-        $wp_query = new WP_Query($args); // don't show user bios
-        $wp_query->is_archive = false; $wp_query->is_home = true;
-        while($wp_query->have_posts()) : $wp_query->the_post(); ?>
-
-      <?php get_template_part( 'frontpost', 'small' ); ?>
-      <?php endwhile; ?>
-      <?php wp_reset_postdata(); // reset the query ?>
-      <?php get_template_part( 'pagination' ); ?>
-    </div>
-
-    <?php get_template_part( 'partials/sidebar' ); ?>
-  </div>
-</section>
-
-<?php get_footer(); ?>
