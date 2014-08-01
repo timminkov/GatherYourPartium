a527fb5894f5c5f9d90dcb56f9771e8b40eaabe9
Tim Minkov <timothyminkov@gmail.com>
2014-08-01 15:36:29 -0700

    wip

---
+++ partials/test.php
@@ -0,0 +1,5 @@
+<img id="background" src="<?php echo site_url(); echo get_option('imageurl'); ?>">
+<img id="logo" src="<?php bloginfo('template_url'); ?>/img/placeholderlogo.png">
+
+<h2>Gather <em>Your</em> Party</h2>
+<div id="strap">Videos</div>
--- videos.php
+++ videos.php
@@ -2,15 +2,13 @@
 /*
 Template Name: Humour
 */
+$test = 'Videos'
 ?>
 
 <?php get_header(); ?>
 
-<img id="background" src="<?php echo site_url(); echo get_option('imageurl'); ?>">
-<img id="logo" src="<?php bloginfo('template_url'); ?>/img/placeholderlogo.png">
-
-<h2>Gather <em>Your</em> Party</h2>
-<div id="strap">Videos</div>
+<?php include(locate_template('partias/test.php')); ?>
+<?php get_template_part( 'partials/test' ); ?>
 
 <section id="main">
   <div class="wrap">
